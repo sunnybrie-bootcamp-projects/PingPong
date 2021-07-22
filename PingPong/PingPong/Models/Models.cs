8f6fefddc684ed0f97c2452ae559f46e90b05036
Brie Klassen <bklassen@stackoverflow.com>
2021-07-21 17:30:15 -0700

    initialize project

---
+++ PingPong/PingPong/Models/Models.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PingPong.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string dateJoined { get; set; }
+    }
+    public class Team
+    {
+    }
+    public class Game
+    {
+    }
+}
